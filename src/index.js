0d7ed0ecd30817f2d7d688ba86ee017f92ec0b83
DashaErmolich <dasha.ermolich@gmail.com>
2022-06-28 16:46:46 +0200

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let string = String(n);
+    let stringReverse = string.split('').reverse().join('');
+    if (stringReverse.startsWith('0')) {
+            return stringReverse.slice(1);
+        } else if (stringReverse.endsWith('-')) {
+            return stringReverse.slice(0, stringReverse.length-1);
+        } else {
+            return stringReverse;
+        }
 }
